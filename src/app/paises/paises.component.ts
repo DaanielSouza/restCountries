9fd231c4543ef67f1d78ff3d4f15df833f708f4f
Daniel Souza avelino <danielavelino@ucl.br>
2022-10-24 22:50:44 -0300

    Criando primeiros componentes

---
+++ src/app/paises/paises.component.ts
@@ -0,0 +1,26 @@
+import { Component, OnInit, Output } from '@angular/core';
+
+@Component({
+  selector: 'app-paises',
+  templateUrl: './paises.component.html',
+  styleUrls: ['./paises.component.sass']
+})
+export class PaisesComponent implements OnInit {
+
+  arrayResult: any[] = [];
+  requestOptions: any = {
+    method: 'GET',
+    redirect: 'follow'
+  };
+
+  result = fetch("https://restcountries.com/v3.1/all", this.requestOptions);
+
+  constructor() { }
+  ngOnInit(): void {
+    this.result.then(r => r.json()).then(paises => paises.forEach((p: any) => {
+      this.arrayResult.push(p);
+    }));
+    console.log(this.arrayResult);
+  }
+
+}
